f1147f35bfb06c83719dca32795cb90f56d7c3cc
Philip Salqvist <philipsalqvist@MacBook-Pro-som-tillhor-Philip.local>
2022-02-25 16:50:05 +0100

    add spring boot initializr files

---
+++ src/test/java/com/psalqvist/petclinic/PetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psalqvist.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
